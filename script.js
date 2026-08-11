524aa11dbaf1a00196ea43592966abe9aa9f8e4b
Lian Dumangas <ldumangas250000000090@uic.edu.ph>
2026-08-11 08:22:58 +0800

    liannnnna

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
+//Menu
 const menuIcon = document.querySelector("#menu-icon");
 const navbar = document.querySelector("#navbar");
 
+//Menu Function
 menuIcon.onclick = () => {
     menuIcon.classList.toggle("fa-xmark");
     navbar.classList.toggle("active");
 };
 
+//Scroll Function
 window.onscroll = () => {
     menuIcon.classList.remove("fa-xmark");
     navbar.classList.remove("active");
 };
 
+//Active Navigation Link
 const navLinks = document.querySelectorAll("#navbar a");
 
 navLinks.forEach(link => {
